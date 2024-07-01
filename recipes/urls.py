1ec35cdf49f2e0210f39e59689f794db6f2c3404
Unix IO <84545108+BertinAm@users.noreply.github.com>
2024-07-01 12:50:43 +0000

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import RecipeListCreate, RecipeDetail, ChatbotAPIView, test_html_view
+from .views import RecipeListCreate, RecipeDetail, ChatbotAPIView, test_html_view, ChatHistoryList
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -23,6 +23,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('chat/', test_html_view, name='chat'),
+    path('chat-history/', ChatHistoryList.as_view(), name='chat-history'),
 ]
 
 
